996e5042d2512cae9c687905cf0bc1a03011c323
David Alejandro Salas Ibarra <david.salas@campusucc.edu.co>
2026-04-13 13:29:23 -0500

    cambios vercel2

--- portafolio/server/index.js
+++
@@ -1,6 +0,0 @@
-// Local email server removed
-// This file previously hosted a small Express server for local testing.
-// The project was converted to a static-only site; server code was
-// intentionally neutralized and left as a harmless placeholder.
-
-module.exports = { status: 'removed' };
